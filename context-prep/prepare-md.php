f28df87ce52c90bdcec29d9bc218a2d7a7f624d7
Dennis Snell <dmsnell@xkq.io>
2025-03-16 17:00:19 +0100

    fixup! Add prep-script for prompts to include /fetch lines

--- context-prep/prepare-md.php
+++ context-prep/prepare-md.php
@@ -8,7 +8,6 @@ $lines = explode( "\n", $md );
 foreach ( $lines as $line ) {
 	if ( str_starts_with( $line, '/fetch ' ) ) {
 		$url = substr( $line, strlen( '/fetch ' ) );
-		echo "\e[90mFetching '\e[34m{$url}\e[90m'\e[m\n";
 		$resource = file_get_contents( $url );
 		$resource = HTML_Inferer::for_context( $resource );
 		echo "\n\n<|FETCH_START:{$url}|>\n{$resource}\n<|FETCH_END:{$url}|>\n\n";
