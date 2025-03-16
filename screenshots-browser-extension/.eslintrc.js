28c27f515b36d39683022982443068026b19fd70
Adam Zieliński <adam@adamziel.com>
2025-03-16 13:33:32 +0100

    Linting setup

--- screenshots-browser-extension/.eslintrc.js
+++ screenshots-browser-extension/.eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-    webextensions: true // This enables the chrome global
-  },
-  extends: [
-    'eslint:recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module'
-  },
-  rules: {
-    // Add any custom rules here
-  }
-}; 
+	env: {
+		browser: true,
+		es2022: true,
+		webextensions: true, // This enables the chrome global
+	},
+	extends: ['eslint:recommended'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		// Add any custom rules here
+		'prefer-const': 'error',
+		'no-var': 'error',
+		'prefer-arrow-callback': 'error',
+		'prefer-template': 'error',
+		'object-shorthand': 'error',
+	},
+};
